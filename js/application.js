47df5ec4ff3a4e0c93429b34f70c5496fe6122b9
Hogue <ajhogue6@gmail.com>
2015-06-02 22:07:33 -0400

    ajax request for neighborhoods working v1

--- js/application.js
+++ js/application.js
@@ -1,17 +1,33 @@
 'use strict'
 
 $(document).ready(function() {
-  console.log('test1')
+  console.log('test1');
 
   $("#dropdownMenu1").on("click", function(event){
-    console.log('test')
+    console.log('test');
 
-    $('.dropdown-toggle').dropdown()
+    $('.dropdown-toggle').dropdown();
 
   });
 
-  $("#Allston").on("click", function(event){
-    console.log('itworks')
+  $("#brighton").on("click", function(event){
+    console.log('itworks');
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/neighborhoods/35/bathrooms'
+      }).done(function(response){
+        // we got a successful response, so we know
+        $('#list_contents').html();
+        var bathroomContent = '';
+        response.forEach(function (bathroom){
+          var adHtml = '<p><b>' + bathroom.location + '</b></br>' + bathroom.address + '</br>' + bathroom.description + '</p>';
+          bathroomContent += adHtml;
+        });
+        $('#list_contents').html(bathroomContent);
+      }).fail(function(){
+        alert('failure');
+      });
+
   });
 
 });
